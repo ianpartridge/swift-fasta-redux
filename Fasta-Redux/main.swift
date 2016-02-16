30f1fa78520846dd9e98819a6841c15f70888398
Ian Partridge <ipartrid@macbook.hursley.uk.ibm.com>
2016-02-16 14:15:39 +0000

    Initial Commit

---
+++ Fasta-Redux/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Fasta-Redux
+//
+//  Created by Ian Partridge on 16/02/2016.
+//  Copyright © 2016 Ian Partridge. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
