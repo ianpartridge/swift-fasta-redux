ba98b009ea330607d284ee4bc61e6e7cd310f2d5
Ian Partridge <i.partridge@uk.ibm.com>
2016-02-29 12:37:17 +0000

    Licence under BSD.

--- Fasta-Redux/main.swift
+++ Fasta-Redux/main.swift
@@ -1,11 +1,31 @@
 //
 //  main.swift
 //  Fasta-Redux
+//  Swift port of Java #3 implementation
 //
 //  Created by Ian Partridge on 16/02/2016.
 //  Copyright © 2016 Ian Partridge. All rights reserved.
 //
-//  Swift port of Java #3 implementation
+//  Redistribution and use in source and binary forms, with or without modification,
+//  are permitted provided that the following conditions are met:
+//
+//  1. Redistributions of source code must retain the above copyright notice, this
+//  list of conditions and the following disclaimer.
+//
+//  2. Redistributions in binary form must reproduce the above copyright notice,
+//  this list of conditions and the following disclaimer in the documentation
+//  and/or other materials provided with the distribution.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+//  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+//  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+//  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+//  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+//  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+//  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+//  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+//  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //
 
 let LINE_LENGTH = 60
@@ -172,7 +192,7 @@ struct RepeatFasta {
     }
 }
 
-var n = 25000000
+var n = 2500000
 if Process.argc > 1 {
     n = Int(Process.arguments[1])!
 }
